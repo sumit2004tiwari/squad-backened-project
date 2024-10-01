7471f47fd8f2cab782f9af963bd526cefad50df9
Sumit Tiwari <sumit2004tiwari@gmail.com>
2024-10-01 17:56:09 +0530

    commit

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,11 +2,11 @@ const { Pool } = require("pg");
 
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "crud-operation",
-    password: "sumit@123",
-    port: 5432,
+    user: process.env.VITE_POSTGRESS_USER,
+    host: process.env.VITE_POSTGRESS_HOST,
+    database: process.env.VITE_POSTGRESS_DATABASE,
+    password: process.env.VITE_POSTGRESS_PASSWORD,
+    port: process.env.VITE_POSTGRESS_PORT,
   });
 
 pool.connect((err) => {
