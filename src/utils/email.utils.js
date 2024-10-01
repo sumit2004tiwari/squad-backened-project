e6a542c8fccebd201bd6d18e2353ee107d09be10
Sumit Tiwari <sumit2004tiwari@gmail.com>
2024-10-01 18:02:49 +0530

    create env file and enter all snsitive file

--- src/utils/email.utils.js
+++ src/utils/email.utils.js
@@ -1,16 +1,18 @@
+const dotenv = require('dotenv');
+ dotenv.config()
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "sumit2004tiwari@gmail.com",
-    pass: "lscw uifo yhur cowt",
+    user: process.env.VITE_NODEMODILER_USER,
+    pass: process.env.VITE_NODEMODILER_PASS,
   },
 });
 
 const sendResetEmail = async (email, resetUrl) => {
   const mailOptions = {
-    from: "sumit2004tiwari@gmail.com",
+    from: process.env.VITE_NODEMODILER_FROM,
     to: email,
     subject: "Password Reset",
     html: `<p>Click <a href="${resetUrl}">here</a> to reset your password.</p>`,
