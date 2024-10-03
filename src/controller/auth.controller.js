ef350a8155ec278e4f8d5c25a35d1f588b4be725
Sumit Tiwari <sumit2004tiwari@gmail.com>
2024-10-03 17:44:51 +0530

    commit

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,20 +1,24 @@
 const pool = require("../config/db.config");
 const crypto = require("crypto");
 const emailService = require("../utils/email.utils");
-const {bcrypt} = require("../utils/bcrypt.utils.js");
+const { bcrypt } = require("../utils/bcrypt.utils.js");
+const bcrypt1 = require('bcrypt')
 
 exports.registerUser = async (req, res) => {
   const { firstname, lastname, email, password } = req.body;
 
   try {
-    const userExists = await pool.query("SELECT * FROM signup WHERE email = $1", [email]);
+    const userExists = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
     if (userExists.rowCount > 0) {
       return res.status(409).json({ message: "User already exists" });
     }
-    const hashedPassword = await bcrypt(password )
+    const hashedPassword = await bcrypt(password);
 
-
-    const query = "INSERT INTO signup (firstname, lastname, email, password) VALUES ($1, $2, $3, $4)";
+    const query =
+      "INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4)";
     await pool.query(query, [firstname, lastname, email, hashedPassword]);
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -23,13 +27,20 @@ exports.registerUser = async (req, res) => {
   }
 };
 
-
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
-
   try {
-    const result = await pool.query("SELECT * FROM signup WHERE email = $1 AND password = $2", [email, password]);
-    if (result.rowCount > 0) {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+  
+    if (result.rowCount === 0) {
+      return res.status(409).json({ message: "Invalid credentials" });
+    }
+
+    const user = result.rows[0];
+
+    const passwordMatch = await bcrypt1.compare(password, user.password);
+
+    if (passwordMatch) {
       res.status(200).json({ message: "Login successful" });
     } else {
       res.status(409).json({ message: "Invalid credentials" });
@@ -40,44 +51,49 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-
 exports.forgotPassword = async (req, res) => {
   const { email } = req.body;
   try {
-    const result = await pool.query("SELECT * FROM signup WHERE email = $1", [email]);
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
     if (result.rowCount === 0) {
       return res.status(409).json({ message: "User not found" });
     }
 
     const resetToken = crypto.randomBytes(32).toString("hex");
     await pool.query(
-      "UPDATE signup SET resetToken = $1, resetToken_expiry = NOW() + INTERVAL '24 hour' WHERE email = $2",
+      "UPDATE users SET resetToken = $1, resetToken_expiry = NOW() + INTERVAL '24 hour' WHERE email = $2",
       [resetToken, email]
     );
 
     const resetUrl = `http://localhost:3000/api/resetPassword/${resetToken}`;
     await emailService.sendResetEmail(email, resetUrl);
 
-    res.status(200).json({ message: "Reset password link sent successfully" , });
+    res.status(200).json({ message: "Reset password link sent successfully" });
   } catch (error) {
     console.error("Error during password reset", error);
     res.status(500).json({ message: "Server error" });
   }
 };
 
-
 exports.resetPassword = async (req, res) => {
   const token = req.params.token;
   const { newPassword } = req.body;
-  const hasPassword = await bcrypt(newPassword  , 10);
+  const hasPassword = await bcrypt(newPassword, 10);
   try {
-    const user = await pool.query("SELECT * FROM signup WHERE resetToken = $1 AND resetToken_expiry > NOW()", [token]);
+    const user = await pool.query(
+      "SELECT * FROM users WHERE resetToken = $1 AND resetToken_expiry > NOW()",
+      [token]
+    );
     if (user.rowCount === 0) {
-      return res.status(404).json({ message: "Invalid or expired reset token" });
+      return res
+        .status(404)
+        .json({ message: "Invalid or expired reset token" });
     }
 
     await pool.query(
-      "UPDATE signup SET password = $1, resetToken = NULL, resetToken_expiry = NULL WHERE resetToken = $2",
+      "UPDATE users SET password = $1, resetToken = NULL, resetToken_expiry = NULL WHERE resetToken = $2",
       [hasPassword, token]
     );
 
@@ -87,5 +103,3 @@ exports.resetPassword = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
-
-
